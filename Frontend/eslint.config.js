d7994f3a18a9e3ecb171548f0debcbdf7488d695
Master Ayush Gautam <ayushgautam2007.kg@gmail.com>
2026-06-23 21:07:14 +0530

    Fix ESLint config by removing unnecessary blank lines for improved readability

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -6,12 +6,13 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
-  
+
   {
     files: ['**/*.{js,jsx}'],
     extends: [
 
       js.configs.recommended,
+      
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
